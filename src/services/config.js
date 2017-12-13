e902e72837df572fade88a534e1220384952687f
Krishna Dalsania <kdalsania@officebrain.com>
2017-12-13 19:42:19 +0530

    change in config

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   credentials: {
     "userAgent" : "Xero Sample App - Krishna",
-    "consumerKey": process.env.consumerKey,
-    "consumerSecret": process.env.consumerSecret,
+    "consumerKey": "O3SILFBTRCWHYUZ89NRKVM0HFRITVF",
+    "consumerSecret": "C5ROZXYERA4I3Q8T4K4UP8AXKYZGEK",
     "privateKeyPath": "./src/services/privatekey.pem"
   }
 };
