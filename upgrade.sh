42bc34fbe5643741c4f99e3b09c3f303ff11e76f
avasani <avasani@officebrain.com>
2018-01-08 15:06:36 +0530

    dev ops file

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,54 @@
+# curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+# -X POST \
+# -H 'Accept: application/json' \
+# -H 'Content-Type: application/json' \
+# -d '{
+#      "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/invoicing_api_backend_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3037:3037/tcp"],"version": "0","environment": {"RDB_HOST": "'"$RDB_HOST"'","RDB_PORT": "'"$RDB_PORT"'","consumerKey":"'"$CONSUMER_KEY"'","consumerSecret":"'"$CONSUMER_SECRET"'","userDetailApi":"'"$USERDETAILAPI"'","rauth":"'"$RAUTH"'","cert":"'"$CERT"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3037,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+# 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s497?action=upgrade'
+
+
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    {
+    echo "call $TRAVIS_BRANCH branch"
+    ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Production" | jq '.data[].id' | tr -d '"'`
+    echo $ENV_ID
+    USERNAME="$DOCKER_USERNAME_FLOWZ";
+    TAG="latest";
+    BASEURL="$BASEURL_MASTER";
+    USERDETAILAPI="$USERDETAILAPI_MASTER";
+  }
+elif [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="dev";
+      BASEURL="$BASEURL_DEVELOP";
+      USERDETAILAPI="$USERDETAILAPI_DEVELOP";
+  }
+else
+  {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="qa";
+      BASEURL="$BASEURL_QA";
+      USERDETAILAPI="$USERDETAILAPI_QA";
+  }
+fi
+
+SERVICE_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=invoicing-api-backend-flowz" | jq '.data[].id' | tr -d '"'`
+echo $SERVICE_ID
+
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$DOCKER_USERNAME'/invoicing_api_backend_flowz:'$DOCKER_IMAGE_TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3037:3037/tcp"],"version": "0","environment": {"RDB_HOST": "'"$RDB_HOST"'","RDB_PORT": "'"$RDB_PORT"'","consumerKey":"'"$CONSUMER_KEY"'","consumerSecret":"'"$CONSUMER_SECRET"'","userDetailApi":"'"$USERDETAILAPI"'","rauth":"'"$RAUTH"'","cert":"'"$CERT"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3037,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s497?action=upgrade'
+     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/invoicing_api_backend_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3037:3037/tcp"],"environment": {"RDB_HOST": "'"$RDB_HOST"'","RDB_PORT": "'"$RDB_PORT"'","baseUrl":"'"$BASEURL"'","userDetailApi":"'"$USERDETAILAPI"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3037,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade
