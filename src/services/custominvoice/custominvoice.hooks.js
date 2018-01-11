2ae9b96c29713b3e405290ead7bf120a0152ec36
avasani <avasani@officebrain.com>
2018-01-11 22:24:35 +0530

    all custom invoice

--- src/services/custominvoice/custominvoice.hooks.js
+++ src/services/custominvoice/custominvoice.hooks.js
@@ -1,11 +1,13 @@
-
-
+let _ = require('lodash');
+let moment = require('moment');
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [
+      hook => beforeCreateInvoice(hook)
+    ],
     update: [],
     patch: [],
     remove: []
@@ -31,3 +33,32 @@ module.exports = {
     remove: []
   }
 };
+
+
+async function beforeCreateInvoice(hook){
+  
+  let invoiceCount =await hook.app.service("custominvoice").find({"settingId" : hook.data.settingId}).then(function(result){
+    console.log("console.log(invoiceCount) ", result.total)
+    return result.total
+  })
+
+  console.log("#DueDate " ,  moment(hook.data.DueDate).format('DD/MM/YYYY'))
+  let incr = invoiceCount+1
+  console.log("#invoiId ", "CINV-00",incr);
+  if(hook.data.products.length > 0){
+    let maximum = _.map(hook.data.products, function(o) { return o.qty * o.amount; });
+    let Total = maximum.reduce(function(maximum, b) { return maximum + b; }, 0);
+    hook.data.DueDate = moment(hook.data.DueDate).format('DD/MM/YYYY');
+    hook.data.Total = Total.toFixed(2);
+    hook.data.Paid = 0;
+    hook.data.Due = Total.toFixed(2);
+    hook.data.Status = 'AUTHORISED';
+    hook.data.Invoice_No = "CINV-00"+incr
+  }else{
+    hook.data.Total = 0
+    hook.data.Paid = 0;
+    hook.data.Due = 0;
+    hook.data.Status = 'AUTHORISED';
+    hook.data.Invoice_No = "CINV-00"+incr
+  }
+}
